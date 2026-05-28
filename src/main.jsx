039cf628c490e5107fb80f535aa21958690d146d
trustdubaiae-ship-it <trustdubai.ae@gmail.com>
2026-05-28 18:15:37 +0400

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './styles/theme.css'
 import { ThemeProvider } from './context/ThemeContext'
@@ -7,8 +8,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
